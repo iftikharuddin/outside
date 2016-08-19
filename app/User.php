0439dc41eaf4d317032eff96fa6441b1e08c2bea
Iftikhar uddin <iftipro@gmail.com>
2016-08-19 22:40:21 +0500

    Format

--- app/User.php
+++ app/User.php
@@ -13,11 +13,11 @@ class User extends Model implements Authenticatable
 	   'image'
    	];
 	
-    public function posts(){
-        return $this->hasMany('App\Post');
-    }
+	 public function posts(){
+	         return $this->hasMany('App\Post');
+	 }
 	
-    public function likes(){
+	 public function likes(){
 		return $this->hasMany('App\Like');
 	}
 	
